43490b15d6ea29b72967f1ef0fb918bd257843bf
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-05-03 16:40:14 +0200

    feat: add 'gradle release' command

---
+++ asciidoc/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("org.gradle.presentation.asciidoctor")
+}
+
+presentation {
+    githubUserName.set("melix")
+    githubRepoName.set("gradle-presentation-template")
+}
+
+
+tasks.register("release", Copy::class.java) {
+    from("./build/asciidoc/revealjs")
+    into("../docs/slides")
+}
+
+afterEvaluate {
+  tasks["release"].dependsOn("asciidoctor")
+}
