e32ed94c5bb47551848f0be5c1e5c4b63c59a805
Aurélien Loyer <aur.loy@gmail.com>
2019-07-22 12:07:09 +0200

    feat: add build watcher 👀

--- generate-slides.js
+++ generate-slides.js
@@ -2,9 +2,12 @@ const util = require('util');
 const fs = require('fs');
 const rimraf = require('rimraf');
 const ncp = require('ncp').ncp;
+const chokidar = require('chokidar');
 const ncpPromise = util.promisify(ncp);
+const args = process.argv.slice(2);
 
 // Configuration 
+const isWatching = args.includes('watch');
 const mainSlideLocation = './slides/asciidoc/index.adoc';
 const outputDir = 'docs/slides/';
 const directoryToCopy = [
@@ -14,27 +17,66 @@ const directoryToCopy = [
     'screencasts',
 ];
 
-// Clean 
-if (fs.existsSync(outputDir)) {
-    rimraf.sync(outputDir);
+if (isWatching) {
+
+    chokidar.watch('slides', {
+        ignored: /(^|[\/\\])\../,
+        persistent: true
+    }).on('all', (event, path) => {
+        runWorkFlow();
+    });
+
+} else {
+    workflow();
 }
 
-// Create slides directory
-fs.mkdirSync(outputDir);
+const runWorkFlow = debounce(function() {
+    console.log('📏 👀  change detected !');
+    workflow();
+}, 2000);
 
-Promise.all(directoryToCopy.map(path => {
+function workflow (){
 
-    ncpPromise(`./slides/${path}`, `${outputDir}${path}`);
+    console.log('🏗  👷‍  start build slides ... 📺');
 
-})).then(() => {
+    // Clean
+    if (fs.existsSync(outputDir)) {
+        rimraf.sync(outputDir);
+    }
 
-    // Load asciidoctor.js and asciidoctor-reveal.js
-    const asciidoctor = require('asciidoctor.js')();
-    const asciidoctorRevealjs = require('asciidoctor-reveal.js');
-    asciidoctorRevealjs.register()
+    // Create slides directory
+    fs.mkdirSync(outputDir);
 
-    // Convert the document 'index.adoc' using the reveal.js converter
-    const options = {safe: 'safe', backend: 'revealjs', to_dir: outputDir};
-    asciidoctor.convertFile(mainSlideLocation, options); // (1)
+    Promise.all(directoryToCopy.map(path => {
 
-})
+        ncpPromise(`./slides/${path}`, `${outputDir}${path}`);
+
+    })).then(() => {
+
+        // Load asciidoctor.js and asciidoctor-reveal.js
+        const asciidoctor = require('asciidoctor.js')();
+        const asciidoctorRevealjs = require('asciidoctor-reveal.js');
+        asciidoctorRevealjs.register()
+
+        // Convert the document 'index.adoc' using the reveal.js converter
+        const options = {safe: 'safe', backend: 'revealjs', to_dir: outputDir};
+        asciidoctor.convertFile(mainSlideLocation, options);
+
+        console.log('🎉 👌  slides successfully generated')
+    })
+}
+
+function debounce(func, wait, immediate) {
+	let timeout;
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
