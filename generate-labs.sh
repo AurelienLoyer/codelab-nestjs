e6b65a7dd880b530bd462ccb66a5ea819d22cde6
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-07-27 18:23:56 +0200

    fix: solve prefix

--- generate-labs.sh
+++ generate-labs.sh
@@ -8,7 +8,7 @@ function setup() {
 
 function build() {
     cat ./labs/LAB0.md ./labs/LAB1.md ./labs/LAB2.md ./labs/LAB3.md ./labs/LAB4.md ./labs/LAB5.md > ./.tmp/index.md
-    claat export -prefix codelab-nestjs/ ./.tmp/index.md
+    claat export -prefix /codelab-nestjs/lab/ ./.tmp/index.md
     pushd ./docs/lab
     claat build
     popd
